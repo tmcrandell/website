33822cbc317ee57f0196824b95a82cde91b2b954
Michael Crandell <m181248@usna.edu>
2018-03-07 16:48:58 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+       $(".destroy").click(function() {
+          $(".ravioli").attr("src","http://media.beam.usnews.com/5a/2f/e915a47e4478944998463052c3eb/170821-nuclearexplosion-stock.jpg");
+       });
+
+       $(".restore").click(function() {
+          $(".ravioli").attr("src","https://www.williams-sonoma.com/wsimgs/rk/images/dp/recipe/201803/0045/img28l.jpg");
+       });
+     });
